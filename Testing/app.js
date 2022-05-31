0a00d1e409b8e31d20bf8adbe9c181043b239069
Angatupyry <fierrofenix@gmail.com>
2022-05-31 07:46:48 -0400

    Adding testing file

--- Testing/app.js
+++ Testing/app.js
@@ -9,10 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.post("/users", async (req, res) => {
   const { password, username } = req.body;
   if (!password || !username) {
-    return res.status(401).json({ data: [], success: false });
+    return res.status(400).json({ data: [], success: false });
   }
 
-  return res.status(201).json({ data: [], success: true });
+  return res.status(200).json({ data: [1], success: true });
 });
 
 // app.use("/", router);
--- Testing/test/users.test.js
+++ Testing/test/users.test.js
@@ -1,15 +1,15 @@
 const request = require("supertest");
 const app = require("../app");
 
-describe("Post Endpoints", () => {
-  it("should create a new post", async () => {
+describe("Prueba de get de usuarios", () => {
+  it("Debería de registrar usuario", async () => {
     const res = await request(app).post("/users").send({
       username: "crolon",
-      password: "test",
+      password: "pass",
     });
-    expect(res.statusCode).toEqual(201);
+    expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty("data");
-    // expect(res.body.data).toHaveLength(1);
     expect(res.body).toHaveProperty("success", true);
+    expect(res.body.data).toHaveLength(1);
   });
 });
