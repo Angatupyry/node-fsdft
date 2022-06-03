4fa904a7da29276d1c709958c4afe8d1e37effae
Angatupyry <fierrofenix@gmail.com>
2022-06-03 08:29:45 -0400

    New ifle

---
+++ unPokemon.js
@@ -0,0 +1,61 @@
+// Tabla pokemon
+// id
+// name
+// info
+
+// tabla elements
+// id
+// name
+// pokemon_id
+
+// tabla about
+// id
+// weight
+// height
+// moves
+
+// tabla baseStats
+// id
+// hp
+// atk
+// satk
+// sdef
+// spd
+
+// const pokemons = [
+//   {
+//     img: aron,
+//     name: "Aron",
+//     id: "#304",
+//     elements: {
+//       element1: "Steel",
+//       element2: "Rock",
+//     },
+//     about: {
+//       weight: "60,0 kg",
+//       height: "0,4 m",
+//       moves: "Sturdy Rock-Head",
+//     },
+//     info: "It eats iron ore- and sometimes railroad tracks - to build up the steel armor that protects its body",
+//     baseStats: {
+//       hp: "050",
+//       atk: "070",
+//       def: "100",
+//       satk: "040",
+//       sdef: "040",
+//       spd: "030",
+//     },
+//   },
+// ];
+
+// pokemon1
+// talba element
+
+// id name pokemon_1
+// 1 Element1      1
+// 2 Element2  1
+// 3 Element1      2
+// 4 Element2  2
+// 5 Element1      3
+// 6 Element1      4
+// 7 Element2  4
